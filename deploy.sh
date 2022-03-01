f1432f6699dfe0ee057fe065e3b690f8db81ee77
technillogue <technillogue@gmail.com>
2022-02-28 20:50:11 -0500

    include deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+set -o xtrace
+git commit -a || exit 1
+echo "0.0.$v" "$(git log -1 --pretty=%B)"
+echo "0.0.$v" "$(git log -1 --pretty=%B)" >> CHANGELOG
+docker build -t technillogue/esrgan:0.0.$v .
+docker push "technillogue/esrgan:0.0.$v"
+sed -i "s#image: technillogue/esrgan:0.0.[[:digit:]]#image: technillogue/esrgan:0.0.$v#g" job.yaml
+#kubectl apply -f job.yaml
